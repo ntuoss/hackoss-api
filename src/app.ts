e0f268a0ed0bbd7877f2639f2bb6491d50038144
Clarence Castillo <clarencecastillo@outlook.com>
2019-01-20 11:11:24 +0800

    Set root to redirect

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import * as express from 'express';
 import { apiRouter } from './routes/api.routes';
 
 const PORT = process.env.PORT;
+const ROOT_URL = process.env.ROOT_URL;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', (req, res) => res.redirect(ROOT_URL));
 app.use('/api', apiRouter);
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
